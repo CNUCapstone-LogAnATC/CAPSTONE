d0eb1088ebed99da861d887d3d1ee2925ed7bc26
Stutz2011 <Ryan.Stutzman.11@cnu.edu>
2016-03-16 12:22:46 -0400

    Create checklogin.php

---
+++ checklogin.php
@@ -0,0 +1,59 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mydb";
+    $tbl_name = "User";
+    $link = mysql_connect($servername, $username, $password, $dbname);
+
+    // Connect to server and select databse.
+    mysql_connect("$servername", "$username", "$password")or die("cannot connect"); 
+    mysql_select_db("$dbname")or die("cannot select DB");
+//echo "1";
+    //username and password sent from form
+   
+        $myusername=$_POST['inputID'];
+        $mypassword=$_POST['inputPassword'];
+       // echo "2";
+
+        //To Protect MySQLinjection
+        $myusername = stripslashes($myusername);
+        $mypassword = stripslashes($mypassword);
+        $myusername = mysql_real_escape_string($myusername);
+        $mypassword = mysql_real_escape_string($mypassword);
+        $sql="SELECT * FROM $tbl_name WHERE userID='$myusername' and password='$mypassword'";
+        $result=mysql_query($sql);
+      
+        // Mysql_num_row is counting table row
+        $count=mysql_num_rows($result);
+        $sqlstatus = "SELECT userStatus FROM $tbl_name WHERE userID='$myusername' and password='$mypassword'";
+            $status = mysql_query($sqlstatus);
+             // $status = mysqli_fetch_field($status);
+
+
+        // If result matched $myusername and $mypassword, table row must be 1 row
+        if($count==1){
+
+            // Register $myusername, $mypassword and redirect to file apporpiate home page
+            $_session["userID"]= $myusername;
+            $_session["mypassword"]= $mypassword; 
+           
+           
+            echo "$status";
+            if($status == "trainer"){
+                header("location:TrainerHome.php");
+                }
+            elseif($status == "Athlete"){
+                    header("location:Athletehome.php");
+                }
+            else{
+                header("location:index.php");
+                
+            }
+        }
+            
+        else {
+            echo "Wrong Username or Password";
+            }
+
+?>

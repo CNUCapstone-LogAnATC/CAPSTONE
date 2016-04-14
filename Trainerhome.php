29fdfdad9da04456c8d51bfc4014e7238a00c695
stevieboose65 <stevie.boose.12@cnu.edu>
2016-04-14 13:30:25 -0400

    Update Trainerhome.php

--- Trainerhome.php
+++ Trainerhome.php
@@ -1,3 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+
+$link = mysql_connect($servername, $username, $password, $dbname);
+
+if (!$link) {
+    die('Could not Connect: ' . mysql_error());
+}
+
+$db_selected = mysql_select_db($dbname, $link); 
+
+        
+if (!$db_selected) {
+    die('Can not use ' . $dbname . ': ' . mysql_error());
+}
+ 
+$sql = "SELECT injury.description, injury.date, injury.treatment, user.firstName, user.lastName FROM injury INNER JOIN user on injury.UserID=user.UserID ORDER BY date DESC LIMIT 10";
+$records = mysql_query($sql);
+
+
+if(!mysql_query($sql))
+{
+    die('Error: ' . mysql_error());
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -28,134 +57,36 @@
       </div>
     </div>
 
-      <h2>Recent Injuries</h2>
-      <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <h2><a href="Injurylog.php">Recent Injuries</a></h2>
+          
+          
+<table class="sortable" width="600" border="10" cellpadding="10" cellspacing="1">
+   
+    <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Description</th>
+    <th>Date</th>
+    
+    </tr>  
+              
+              
+<?php
+    
+while ($Injury=mysql_fetch_array($records))
+{
+    echo "<form action=injurylog.php method=post>";
+    echo "<tr>";
+    
+echo "<td>"  . $Injury['firstName'] . " </td>";
+echo "<td>"  . $Injury['lastName'] . " </td>";
+echo "<td>"  . $Injury['description'] . " </td>";
+echo "<td>"  . $Injury['date'] . " </td>";
+    echo "</tr>";
+    echo "</form>";
+}
+?>
+          </table>
       
       <h2>Calender</h2>
 
@@ -277,3 +208,5 @@ document.write(cal);
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
+
+        

a7e7e5fa58e00c44fc414bbcd041701d83b99770
stevieboose65 <stevie.boose.12@cnu.edu>
2016-04-14 13:49:56 -0400

    Update Injurylog.php

--- Injurylog.php
+++ Injurylog.php
@@ -39,7 +39,7 @@ if(!mysql_query($sql))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-      
+<script src="sorttable.js"></script>
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">

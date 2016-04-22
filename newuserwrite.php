ea8eb7394c319d81ef1a94d6de0b4be332a9440f
stevieboose65 <stevie.boose.12@cnu.edu>
2016-04-22 00:03:18 -0400

    Update newuserwrite.php

--- newuserwrite.php
+++ newuserwrite.php
@@ -11,21 +11,20 @@ $db_selected = mysql_select_db($dbname, $link);
 if (!$db_selected) {
     die('Can not use ' . $dbname . ': ' . mysql_error());
 }
-
-
-
-            $valueId = $_POST['inputID'];
+              $valueId = $_POST['inputID'];
               $valueFN = $_POST['firstName'];
               $valueLN = $_POST['lastName'];
               $valueP = $_POST['Password'];
               $valueUS = $_POST['inputUserType'];
-echo $valueID;
- $sql = "INSERT INTO USER (userID, firstName, lastName, password, userStatus) VALUES ('$valueId', '$valueFN', '$valueLN', '$valueP', '$valueUS')";
+              $valueTM = $_POST['inputTeam'];
 
-//       if($result==0){
-//           header("location:index.php");
+
+ $sql = "INSERT INTO USER (userID, firstName, lastName, password, userStatus, team) VALUES ('$valueId', '$valueFN', '$valueLN', '$valueP', '$valueUS', '$valueTM')";
+
+echo header("location:index.php");
 //       }
-
+//       if($result==0){
+           
 if(!mysql_query($sql));
 {
     die('Error: ' . mysql_error());
